76e75899a9aacf8aab691efcec8bd2d4a6ab08c5
ayvengoza <ayvengoza@gmail.com>
2017-09-06 17:46:29 +0300

    addAnimation, show android version, add hint counter

--- app/src/main/java/com/ayvengoza/quizapplication/quizapplication/CheatActivity.java
+++ app/src/main/java/com/ayvengoza/quizapplication/quizapplication/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.ayvengoza.quizapplication.quizapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,11 +20,15 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String KEY_IS_SHOWN = "is_answer_shown";
 
+    private static int numOfHints = 3;
+
     private boolean mAnswerIsTrue;
     private boolean mIsAnswerShown;
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
+    private TextView mVersionTextView;
+    private TextView mNumHintsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +43,46 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mVersionTextView = (TextView) findViewById(R.id.version_text_view);
+        mNumHintsTextView = (TextView) findViewById(R.id.num_himts_text_view);
+
+        int version = Build.VERSION.SDK_INT;
+
+        mVersionTextView.setText("API Level " + version);
+        updateHintConter();
+
+        if(mIsAnswerShown){
+            mShowAnswerButton.setVisibility(View.INVISIBLE);
+            checkShowCheat();
+        }
 
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mAnswerIsTrue){
-                    mAnswerTextView.setText(R.string.true_button);
-                } else {
-                    mAnswerTextView.setText(R.string.false_button);
+                if (numOfHints > 0) {
+                    numOfHints--;
+                    mIsAnswerShown = true;
+                    checkShowCheat();
+                    setAnswerShownResult();
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        int cx = mShowAnswerButton.getWidth() / 2;
+                        int cy = mShowAnswerButton.getHeight() / 2;
+                        float radius = mShowAnswerButton.getWidth();
+                        Animator anim = ViewAnimationUtils
+                                .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                        anim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                mShowAnswerButton.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                        anim.start();
+                    } else {
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
                 }
-                mIsAnswerShown = true;
-                setAnswerShownResult();
             }
         });
 
@@ -56,6 +93,21 @@ public class CheatActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(KEY_IS_SHOWN, mIsAnswerShown);
+    }
+
+    private void checkShowCheat(){
+        if(mIsAnswerShown) {
+            if (mAnswerIsTrue) {
+                mAnswerTextView.setText(R.string.true_button);
+            } else {
+                mAnswerTextView.setText(R.string.false_button);
+            }
+        }
+        updateHintConter();
+    }
+
+    private void updateHintConter(){
+        mNumHintsTextView.setText("You have " + numOfHints + " hints");
     }
 
     private void setAnswerShownResult(){
